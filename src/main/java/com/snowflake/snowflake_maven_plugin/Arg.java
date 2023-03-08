835a5c6a77cca9d98c5b9a85b61b51bd900617f4
Jonathan Cui <jonathan.cui@snowflake.com>
2023-03-08 15:22:22 -0500

    feedback after format

--- src/main/java/com/snowflake/snowflake_maven_plugin/Arg.java
+++ src/main/java/com/snowflake/snowflake_maven_plugin/Arg.java
@@ -3,49 +3,50 @@ package com.snowflake.snowflake_maven_plugin;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * A class to capture user configuration of function/procedure arguments from the POM file
- */
+/** A class to capture user configuration of function/procedure arguments from the POM file */
 public class Arg {
-    public String name;
-    public String type;
-    @Override
-    public String toString() {
-        return name + " " + type;
-    }
+  public String name;
+  public String type;
 
-    public boolean hasNull() {
-        return name == null || type == null;
-    }
+  @Override
+  public String toString() {
+    return name + " " + type;
+  }
 
-    /**
-     * This factory method is used to parse UDF/procedure arguments supplied on the command line
-     * e.g. mvn snowflake-udx:deploy ... -Ddeploy.args="a string, b string" ...
-     * The input to this method will be "a string, b string"
-     * And it will output and object array [Arg(a, string), Arg(b, string)]
-     * We cannot make a new constructor or else Maven plugin will not be able to instantiate args object
-     */
-    public static Arg[] fromString(String argsString) {
-        if (argsString == null) {
-            throw new IllegalArgumentException("The arguments of the function/procedure are missing. If there are no arguments, supply an empty string.");
-        }
-        if (argsString.length() == 0) {
-            return new Arg[]{};
-        }
-        String[] stringArgs = argsString.split(",");
-        List<Arg> result = new ArrayList<>();
+  public boolean hasNull() {
+    return name == null || type == null;
+  }
 
-        for (String stringArg : stringArgs) {
-            stringArg = stringArg.trim();
-            String[] stringArgSplit = stringArg.split(" +");
-            if (stringArgSplit.length != 2) {
-                throw new IllegalArgumentException(String.format("Malformed argument: %s, from args: %s", stringArg, argsString));
-            }
-            Arg arg = new Arg();
-            arg.name = stringArgSplit[0];
-            arg.type = stringArgSplit[1];
-            result.add(arg);
-        }
-        return result.toArray(new Arg[result.size()]);
+  /**
+   * This factory method is used to parse UDF/procedure arguments supplied on the command line e.g.
+   * mvn snowflake-udx:deploy ... -Ddeploy.args="a string, b string" ... The input to this method
+   * will be "a string, b string" And it will output and object array [Arg(a, string), Arg(b,
+   * string)] We cannot make a new constructor or else Maven plugin will not be able to instantiate
+   * args object
+   */
+  public static Arg[] fromString(String argsString) {
+    if (argsString == null) {
+      throw new IllegalArgumentException(
+          "The arguments of the function/procedure are missing. If there are no arguments, supply an empty string.");
     }
+    if (argsString.length() == 0) {
+      return new Arg[] {};
+    }
+    String[] stringArgs = argsString.split(",");
+    List<Arg> result = new ArrayList<>();
+
+    for (String stringArg : stringArgs) {
+      stringArg = stringArg.trim();
+      String[] stringArgSplit = stringArg.split(" +");
+      if (stringArgSplit.length != 2) {
+        throw new IllegalArgumentException(
+            String.format("Malformed argument: %s, from args: %s", stringArg, argsString));
+      }
+      Arg arg = new Arg();
+      arg.name = stringArgSplit[0];
+      arg.type = stringArgSplit[1];
+      result.add(arg);
+    }
+    return result.toArray(new Arg[result.size()]);
+  }
 }
--- src/test/java/com/snowflake/snowflake_maven_plugin/core/SnowflakeBuilderTest.java
+++ src/test/java/com/snowflake/snowflake_maven_plugin/core/SnowflakeBuilderTest.java
@@ -1,34 +1,40 @@
 package com.snowflake.snowflake_maven_plugin.core;
-import org.apache.maven.plugin.logging.Log;
-import org.junit.Test;
-import org.mockito.Mock;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasEntry;
 import static org.junit.Assert.assertEquals;
 
+import org.apache.maven.plugin.logging.Log;
+import org.junit.Test;
+import org.mockito.Mock;
+
 public class SnowflakeBuilderTest {
 
-    @Mock
-    private Log log;
-    @Test
-    public void testConfig() {
-        SnowflakeBuilder sb = new SnowflakeBuilder(log);
-        // config should set options
-        sb.config("name", "functionName");
-        assertThat(sb.options, hasEntry("name", "functionName"));
-        // config should overwrite existing options and use lowercase keys
-        sb.config("NAME", "otherName");
-        assertThat(sb.options, hasEntry("name", "otherName"));
-        // config should set url and account with formatting
-        sb.config("account", "myaccount");
-        assertEquals(sb.url, "jdbc:snowflake://myaccount.snowflakecomputing.com:443");
-        String[] urls = new String[]{"myaccount.snowflakecomputing.com", "  myaccount.snowflakecomputing.com",
-                "myaccount.snowflakecomputing.com:443"};
-        for (String url : urls) {
-            sb.config("url", url);
-            assertEquals("jdbc:snowflake://myaccount.snowflakecomputing.com:443", sb.url);
-        }
-        sb.config("url", "https://myaccount.snowflakecomputing.com");
-        assertEquals(sb.url, "jdbc:snowflake://https://myaccount.snowflakecomputing.com:443");
+  @Mock private Log log;
+
+  @Test
+  public void testConfig() {
+    SnowflakeBuilder sb = new SnowflakeBuilder(log);
+    // config should set options
+    sb.config("name", "functionName");
+    assertThat(sb.options, hasEntry("name", "functionName"));
+    // config should overwrite existing options and use lowercase keys
+    sb.config("NAME", "otherName");
+    assertThat(sb.options, hasEntry("name", "otherName"));
+    // config should set url and account with formatting
+    sb.config("account", "myaccount");
+    assertEquals(sb.url, "jdbc:snowflake://myaccount.snowflakecomputing.com:443");
+    String[] urls =
+        new String[] {
+          "myaccount.snowflakecomputing.com",
+          "  myaccount.snowflakecomputing.com",
+          "myaccount.snowflakecomputing.com:443"
+        };
+    for (String url : urls) {
+      sb.config("url", url);
+      assertEquals("jdbc:snowflake://myaccount.snowflakecomputing.com:443", sb.url);
     }
+    sb.config("url", "https://myaccount.snowflakecomputing.com");
+    assertEquals(sb.url, "jdbc:snowflake://https://myaccount.snowflakecomputing.com:443");
+  }
 }
