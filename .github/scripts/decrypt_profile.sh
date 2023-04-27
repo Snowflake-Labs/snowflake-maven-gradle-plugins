5b616442080103888d3f60e7ae406748585df738
Jonathan Cui <jonathan.cui@snowflake.com>
2023-04-27 19:36:28 -0400

    decrypt profile properties for gradle plugin

--- .github/scripts/decrypt_profile.sh
+++ .github/scripts/decrypt_profile.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 gpg --quiet --batch --yes --decrypt --passphrase="$PROFILE_PASSWORD" --output snowflake-maven-plugin/src/it/profile.properties profile.properties.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$PROFILE_PASSWORD" --output snowflake-gradle-plugin/src/it/profile.properties profile.properties.gpg
